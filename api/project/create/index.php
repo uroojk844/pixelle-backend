61639b6edd21235d65a78df515c2d59da3bdde1d
Urooj Khan <uroojk844@gmail.com>
2025-02-08 15:50:35 +0530

    [ADD] adding create project,get projects api and some config changes

---
+++ api/project/create/index.php
@@ -0,0 +1,52 @@
+<?php
+include_once("../../../config.php");
+
+$headers = apache_request_headers();
+
+if(isset($_POST['name']) && isset($_FILES['files']) && isset($headers['Authorization'])){
+    $user = explode("|",$headers['Authorization']);
+    $userid = $user[1];
+    $title = $_POST['name'];
+    $tags = $_POST['tags'];
+
+    // uploading files
+    if(isset($_FILES['files'])){
+        $dir = "../../../ui";
+        if(!file_exists($dir)) mkdir($dir);
+        
+        $projectID = uniqid();
+        $filepath = "$dir/$projectID";
+        $files = array();
+
+        $uploaded = false;
+        if(!file_exists($filepath)){
+            mkdir($filepath, true);
+            for ($file=0; $file < count($_FILES['files']['name']); $file++) {
+                $name = $_FILES['files']['name'][$file];
+                $files[] = $name;
+                $temp_name = $_FILES['files']['tmp_name'][$file];
+                if(move_uploaded_file($temp_name, "$filepath/$name")){
+                    $uploaded = true;
+                }else $uploaded = false;
+            }
+            if($uploaded){
+                // updating in data base
+                $files = implode(",",$files);
+                $query = "INSERT INTO `projects`(`id`, `userid`, `files`, `name`, `tags`) values('$projectID', '$userid', '$files', '$title', '$tags')";
+                if(mysqli_query($conn, $query)) {
+                    echo json_encode(['success'=>['message'=>'File uploaded successfully!','id'=>$projectID]]);
+                }else{
+                    echo json_encode(['error'=>'Something went wrong!']);
+                }
+            }else{
+                echo json_encode(['error'=>'File not uploaded!']);
+            }
+        }else{
+            echo json_encode(['error'=>'Folder already exists!']);
+        }
+    }
+}else{
+    print_r($data['name']);
+    echo json_encode(['error'=>'Required fileds are missing!']);
+}
+?>
