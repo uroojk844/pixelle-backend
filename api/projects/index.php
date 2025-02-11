2bc60878e53b148290c732643061fd31bb3bf1a7
Urooj Khan <uroojk844@gmail.com>
2025-02-11 13:02:41 +0530

    [ADD] adding delete project api, updating projects api, updating update api, fixed header issue which was causing issue which icons

--- api/projects/index.php
+++ api/projects/index.php
@@ -1,26 +1,25 @@
 <?php
-    $method='GET';
-    include_once("../../config.php");
-    
-    $query = "SELECT projects.*,users.name as user,picture  FROM `projects`,`users` where visibility='public' and userid=users.id";
+$method = 'GET';
+include_once("../../config.php");
+include_once("../../user_header.php");
 
-    if(isset($_GET['user'])){
-        $userid = $_GET['user'];
-        $query = "SELECT projects.*,users.name as user,picture  FROM `projects`,`users` where visibility='public' and userid=users.id and userid='$userid'";
-    }
-    else if(isset($_GET['id'])){
-        $id = $_GET['id'];
-        $query = "SELECT projects.*,users.name as user,picture  FROM `projects`,`users` where visibility='public' and userid=users.id and projects.id='$id'";
-    }
+$query = "SELECT projects.*,users.name as user,picture  FROM `projects`,`users` where visibility='public' and userid=users.id";
 
-    $results = array();
-    $res = mysqli_query($conn, $query);
-    if(mysqli_num_rows($res)){
-        while($row = mysqli_fetch_assoc($res)) {
-            $results[] = $row;
-        }
-        echo json_encode(['success'=> $results]);
-    }else{
-        echo json_encode(['success'=> 'No result found!']);
+if (isset($_GET['user'])) {
+    $userid = $_GET['user'];
+    $query = "SELECT projects.*,users.name as user,picture  FROM `projects`,`users` where userid=users.id and userid='$userid'";
+} else if (isset($_GET['id'])) {
+    $id = $_GET['id'];
+    $query = "SELECT * FROM `projects` where `userid`='$userid' and `id`='$id'";
+}
+
+$results = array();
+$res = mysqli_query($conn, $query);
+if (mysqli_num_rows($res)) {
+    while ($row = mysqli_fetch_assoc($res)) {
+        $results[] = $row;
     }
-?>
+    echo json_encode(['success' => $results]);
+} else {
+    echo json_encode(['success' => 'No result found!']);
+}
