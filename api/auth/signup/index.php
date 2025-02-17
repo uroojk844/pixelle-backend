09c51821c4ab91ddeb5d827f11c283df9e2fb61b
Urooj Khan <uroojk844@gmail.com>
2025-02-17 19:30:30 +0530

    [UPDATE] updating .gitigore

--- api/auth/signup/index.php
+++ api/auth/signup/index.php
@@ -1,38 +1,34 @@
 <?php
-    include_once("../../../config.php");
-    require("../../../utils.php");
+include_once("../../../config.php");
+require("../../../utils.php");
 
-    $fields = array("fullname","email","password","id");
-    
-    if(validateInput($data,$fields)){
-        $email = $data['email'];
-        $password = $data['password'];
-        $name = $data['fullname'];
-        $id = $data['id'];
-        
-        //checking is account exists already
-        $checkExists = "SELECT * FROM users where email='$email'";
-        $result = $conn->query($checkExists);
-        $rowCount = mysqli_num_rows($result);
+$fields = array("fullname", "email", "password", "id");
 
-        if($rowCount>0) {echo json_encode(["error"=>"This email is already in use!"]);}
-        
-        else{
-            //generating username
-            $username =  str_replace(" ","",$name).rand(100,999);
-            $insertQuery = "INSERT INTO users(`id`,`name`,`username`,`email`,`password`) values ('$id','$name','$username','$email','$password')";
-            //save user
-            if($conn->query($insertQuery)){
-                sendRegistrationEmail($email,$name);
-                echo json_encode(["success"=>"Account created!"]);
-            }   
-            else {
-                echo json_encode(["error"=>"Something went wrong!"]);
-            }
+if (validateInput($data, $fields)) {
+    $email = $data['email'];
+    $password = $data['password'];
+    $name = $data['fullname'];
+    $id = $data['id'];
+
+    //checking is account exists already
+    $checkExists = "SELECT * FROM users where email='$email'";
+    $result = $conn->query($checkExists);
+    $rowCount = mysqli_num_rows($result);
+
+    if ($rowCount > 0) {
+        echo json_encode(["error" => "This email is already in use!"]);
+    } else {
+        //generating username
+        $username =  str_replace(" ", "", $name) . rand(100, 999);
+        $insertQuery = "INSERT INTO users(`id`,`name`,`username`,`email`,`password`) values ('$id','$name','$username','$email','$password')";
+        //save user
+        if ($conn->query($insertQuery)) {
+            sendRegistrationEmail($email, $name);
+            echo json_encode(["success" => "Account created!"]);
+        } else {
+            echo json_encode(["error" => "Something went wrong!"]);
         }
-        
     }
-    else{
-        echo json_encode(["error"=>"A required field is missing"]);
-    }
-?>
+} else {
+    echo json_encode(["error" => "A required field is missing"]);
+}
